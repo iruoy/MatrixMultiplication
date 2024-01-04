f936158165d71cd469c03c35796292078c99d870
github-action-benchmark <github@users.noreply.github.com>
2024-01-04 03:11:42 +0000

    add MatrixMultiplication (benchmarkdotnet) benchmark result for 093f2d5d996c89800069bf297ed536646bac1ee5

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1704337902173,
+  "repoUrl": "https://github.com/iruoy/MatrixMultiplication",
+  "entries": {
+    "MatrixMultiplication": [
+      {
+        "commit": {
+          "author": {
+            "email": "youri@wijnands.me",
+            "name": "Youri Wijnands",
+            "username": "iruoy"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "093f2d5d996c89800069bf297ed536646bac1ee5",
+          "message": "Update benchmark.yml",
+          "timestamp": "2024-01-04T04:09:53+01:00",
+          "tree_id": "7b72951e1c5b8dc46d9a911f12ba023d039111c8",
+          "url": "https://github.com/iruoy/MatrixMultiplication/commit/093f2d5d996c89800069bf297ed536646bac1ee5"
+        },
+        "date": 1704337901677,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "MatrixMultiplication.MatrixMultiplication.Multiply",
+            "value": 2170348500.142857,
+            "unit": "ns",
+            "range": "± 4174205.9580502408"
+          },
+          {
+            "name": "MatrixMultiplication.MatrixMultiplication.ParallelMultiply",
+            "value": 1064197358.4166666,
+            "unit": "ns",
+            "range": "± 1251231.8785657494"
+          }
+        ]
+      }
+    ]
+  }
+}
